3ba74866495c27ff34f47debad3b61a92072ea71
MAY55A <107711995+MAY55A@users.noreply.github.com>
2024-05-18 20:17:44 +0100

    Update KutubyApplication.java

--- src/main/java/org/example/KutubyApplication.java
+++ src/main/java/org/example/KutubyApplication.java
@@ -23,21 +23,6 @@ public class KutubyApplication {
         SpringApplication.run(KutubyApplication.class, args);
     }
 
-
-
-    @Bean
-    CommandLineRunner commandLineRunnerUserDetails(AccountService accountService){
-        return args-> {
-            accountService.addNewRole("USER");
-            accountService.addNewRole("ADMIN");
-            accountService.addNewUser("user1", "1234", "user1@gmail.com", "1234");
-            accountService.addNewUser("user2", "1234", "user2@gmail.com", "1234");
-            accountService.addNewUser("admin", "1234", "admin@gmail.com", "1234");
-            accountService.addRoleToUser("user1","USER");
-            accountService.addRoleToUser("user2","USER");
-            accountService.addRoleToUser("admin","USER");
-            accountService.addRoleToUser("admin","Admin");
-
     //@Bean
     CommandLineRunner commandLineRunnerUserDetails(UserService userService, RoleService roleService){
         return args-> {
